2cce29dab3816b7e267fa0a6128fe789cf52b92d
Marku <93548824+markumed1@users.noreply.github.com>
2022-01-18 12:52:08 -1000

    Update main.js

--- Basic-JavaScript-Projects/Project2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -1,10 +1,10 @@
 function plusEquals () {
-    var x = 3;
+    var x = 3;                  //add the variable x: out put of x is 5//
     x += 2;
     document.getElementById("demo1").innerHTML = x;
 
 }
 
-function myFunction() {
+function myFunction() {             //function that will output "Hello World" in a p element with id="demo2"//
     document.getElementById("demo2").innerHTML = "Hello white space";
 }
