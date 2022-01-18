af463400f3a81a4978edb6d77cd4173e0dec6dcc
Marku <93548824+markumed1@users.noreply.github.com>
2022-01-18 12:42:19 -1000

    updating folder

--- Basic-JavaScript-Projects/Project2_functions/JS/main.js
+++ Basic-JavaScript-Projects/Project2_functions/JS/main.js
@@ -1,5 +1,10 @@
 function plusEquals () {
     var x = 3;
     x += 2;
-    document.getElementById("demo").innerHTML = x;
+    document.getElementById("demo1").innerHTML = x;
+
+}
+
+function myFunction() {
+    document.getElementById("demo2").innerHTML = "Hello white space";
 }
