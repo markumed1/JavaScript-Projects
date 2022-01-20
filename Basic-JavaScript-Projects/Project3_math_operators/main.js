8a90f7f2c4f4c701c332f2f7cb9f255c6bd8df55
Marku <93548824+markumed1@users.noreply.github.com>
2022-01-19 18:10:59 -0800

    updated

--- Basic-JavaScript-Projects/Project3_math_operators/main.js
+++ Basic-JavaScript-Projects/Project3_math_operators/main.js
@@ -15,12 +15,24 @@ function round() {
     document.getElementById("round").innerHTML = "25.8 " + Math.round(25.8);}
 
 
-//Modulas operator
-function Modulas_Operator() {
-    var simple_Math = 25 % 6;
-    document.getElementById("modulas").innerHTML = + simple_Math; //when you divide 25 by 6 you have a remainder of://
+//Multiplication and Division
+function Multiplication() {
+    var simple_Math = 6 * 9;
+    document.getElementById("Multiplication").innerHTML = "6 x 8 = " + simple_Math;
 }
 
+//division
+function division() {
+    var simple_Math = 48 / 6;
+    document.getElementById("division").innerHTML = "48 / 6 = " + simple_Math;
+}
+
+//Multiple Operations
+function more_Math() {
+    var simple_Math = (1 + 2) * 10  / 2 -5;
+    document.getElementById("operation").innerHTML = + simple_Math; //1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals//
+
+}
 
 // Increment and decrement op
 var Incrementnumber = 5;
@@ -37,6 +49,9 @@ function Decrement() {
     document.getElementById("Decrement").innerHTML = Decrementnumber;
 }
    
+
+// //math.random()
+// document.getElementById("random").innerHTML = Math.random;
 
 
 console.log(document.getElementById("Math")) 
