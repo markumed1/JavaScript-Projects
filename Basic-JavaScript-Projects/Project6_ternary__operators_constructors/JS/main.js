71fd0d655bc2b1bc94f92d6328ac414f75d9a304
Marku <93548824+markumed1@users.noreply.github.com>
2022-01-21 11:21:40 -0800

    Pro6 submission assignment

--- Basic-JavaScript-Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic-JavaScript-Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -1,31 +1,33 @@
-// function Vehicle (Make, Model, Year, Color) {
-//     this.Vehicle_Make = Make;
-//     this.Vehicle_Model = Model;
-//     this.Vehicle_Year = Year;
-//     this.Vehicle_Color = Color;
-// }
-
-// var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
-// var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
-// var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
-// function myFunction() {
-//     document.getElementById("Keywords_and_Constructors").innerHTML = 
-//     "Erik Drives a " + Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model +
-//     " manufactured in " + Erik.Vehicle_Year;
-// }
-
-// Constructor function for Persons obejects
-class Person {
-    constructor(first, last, age, eye) {
-        this.firstName = first;
-        this.lastName = last;
-        this.age = age;
-        this.eyeColor = eye;
+/*=======================================
+    Constructor function "new" and "This"
+=========================================*/
+class Vehicle {
+    constructor(Make, Model, Year, Color) {
+        this.Vehicle_Make = Make;
+        this.Vehicle_Model = Model;
+        this.Vehicle_Year = Year;
+        this.Vehicle_Color = Color;
     }
 }
 
-const myFather = new Person("John", "Doe", 50, "blue");
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML = 
+    "Erik Drives a " + Erik.Vehicle_Color + "-colored" + Erik.Vehicle_Model +
+    " manufactured in " + Erik.Vehicle_Year;
+}
 
-//Display age
-document.getElementById("display").innerHTML = 
-"My father is " + myFather.age + "50";
+
+  
+/*===============================
+    Nested Function
+=================================*/
+document.getElementById("Nested_Function").innerHTML = add();
+function add() {
+    let counter = 0;
+    function plus () {counter += 1;}
+    plus();
+    return counter;
+}
