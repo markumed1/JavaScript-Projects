bef1c591a0f993677f1a47f929bb0f583a04c378
Marku <93548824+markumed1@users.noreply.github.com>
2022-01-25 09:49:16 -0800

    updated and debugged

--- Basic-JavaScript-Projects/Project_9_countdown_slideshow/JS/main.js
+++ Basic-JavaScript-Projects/Project_9_countdown_slideshow/JS/main.js
@@ -20,7 +20,7 @@ function Countdown() {
 /*==========================================
         How to slideshow
 ============================================*/
-var slideIndex = 1
+var slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
@@ -37,13 +37,13 @@ function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > slideIndex.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {slideIndex = 1;}
+    if (n < 1) {slideIndex = slides.length;}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace("acive", "");
+        dots[i].className = dots[i].className.replace(" acive", "");
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
